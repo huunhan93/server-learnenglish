159f1d28c928a24e02731eb55415bdafdcbf5298
huunhan93 <nhan111321@gmail.com>
2019-10-18 09:55:20 +0700

    fix index.js

--- index.js
+++ index.js
@@ -3,47 +3,91 @@ var app = express();
 
 //body-parser
 var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 //Using JSON
 app.use(bodyParser.json())
-
+//Cors
+var cors = require('cors')
+app.use(cors())
 //postgresql
-var {Pool, Client} = require("pg");
+var { Pool } = require("pg");
 var pool = null;
-function connectPG(){
+const connectionString = 'postgres://vgjtpjmubfripr:d73f2f53ba4c037b40a5847364b535b17386ac0ce0aa068c091bb976dfe6c344@ec2-54-204-37-92.compute-1.amazonaws.com:5432/d8vgti02tvffbm'
+
+function connectPG() {
+    // pool = new Pool({
+    //     user: 'vgjtpjmubfripr',
+    //     host: 'ec2-54-204-37-92.compute-1.amazonaws.com',
+    //     database: 'd8vgti02tvffbm',
+    //     password: 'd73f2f53ba4c037b40a5847364b535b17386ac0ce0aa068c091bb976dfe6c344',
+    //     port: 5432,
+    // });
     pool = new Pool({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'LearnEnglish',
-        password: '1234',
-        port: 5432,
-    });
-    pool.connect();
-}
-var client = null
-function connectHeruku(){
-    client = new Client({
-        connectionString: "postgres://xediuqruedbglt:d08ef8a0864d91b3f406709fe3c5f6ac4dc1c8b6bf389eb8bd0edc7365d2457f@ec2-23-21-148-223.compute-1.amazonaws.com:5432/d518t1utp6ndsi",
-        ssl: true,
-      });
-      client.connect();
+        connectionString: connectionString,
+        ssl: true
+    })
 }
 
 app.listen(process.env.PORT || 3002);
 
-app.get("/getword", function(req, res){
-    //res.send("Hello world 1!");
-    connectHeruku();
-    var query = "SELECT * FROM word";
-    client.query(query, function(err, resPG){
-        if(err) {
-            res.json({"err" : err})
-            
-        }else{
-            res.json({"arrWord" : resPG.rows})
+app.get('/', function(req, res){
+    res.send('Hello world!')
+})
+
+app.get("/getword", function (req, res) {
+    connectPG();
+    pool.query('SELECT * FROM public.word', (err, resPG) => {
+        if (err) {
+            res.json({ err: err })
+        } else {
+            res.json({ "kq": resPG.rows })
         }
-        
-        client.end();
-    });
-    
+        pool.end()
+    })
+})
+app.post("/insertword", function(req, res){
+    var en = req.body.En;
+    var vn = req.body.Vn;
+    connectPG();
+    pool.query(`INSERT INTO public.word(
+        "En", "Vn", "IsMemorized")
+        VALUES ( '`+en+`', '`+vn+`', true);`, (err, resPG) => {
+        if (err) {
+            res.json({ "kq": 0 }) // Thêm thất bại
+        } else {
+            res.json({ "kq": 1 }) // Thêm thất bại
+        }
+        pool.end()
+    })
+})
+app.post("/updateword", function (req, res) {
+    var id = req.body.Id;
+    connectPG();
+    pool.query(`UPDATE public.word
+	SET "IsMemorized"= not "IsMemorized"
+	WHERE "Id" = `+ id + `;`, (err, resPG) => {
+        //console.log(err, res)
+        if (err) {
+            res.json({ "kq": 0 }) // Cập nhật thất bại
+        } else {
+            res.json({ "kq": 1 }) // Cập nhật thành công
+        }
+        pool.end()
+    })
+});
+
+app.post("/deleteword", function (req, res) {
+    //res.send("Hello world 1!");
+    var id = req.body.Id;
+    connectPG();
+    pool.query(`DELETE FROM public.word
+	WHERE "Id" = `+id+`;`, (err, resPG) => {
+        //console.log(err, res)
+        if (err) {
+            res.json({ "kq": 0 }) // Xóa thất bại
+        } else {
+            res.json({ "kq": 1 }) // xóa thành công
+        }
+        pool.end()
+    })
 })
